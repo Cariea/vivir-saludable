52516a2ceaff821b528a8282873022da5628f6dc
Tania Gutierrez <riobuenodevelops@gmail.com>
2024-03-16 22:38:55 -0400

    Removed tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,65 +1,65 @@
 // TODO: Remove tailwind to be replaced by Material UI
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        // Or if using `src` directory:
-        "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        fontFamily: {
-            sans: ["Ubuntu", "sans-serif"],
-        },
-        extend: {
-            colors: {
-                primary: "#003F52",
-                secondary: "#32C5A3",
-                white: "#FFFFFF",
-                "white-dark": "#F5F9FF",
-                black: "#18232F",
-                gray: {
-                    50: "#EEF2F6",
-                    100: "#DDE5EE",
-                    200: "#BBCBDD",
-                    300: "#9AB2CB",
-                    400: "#7898BA",
-                    500: "#456587",
-                    600: "#344C65",
-                    700: "#18232F",
-                    800: "#111922",
-                    900: "#090D11",
-                },
-            },
-            boxShadow: {
-                base: "0px 18px 50px -10px rgba(120, 152, 186, 0.3)",
-            },
-            borderRadius: {
-                "8xl": "3rem",
-            },
-            height: {
-                "90": "90%"
-            }
-        },
-    },
-    plugins: [require("daisyui")],
-    daisyui: {
-        themes: [
-            {
-                vs: {
-                    fontSize: "1rem",	
-                    primary: "#003F52",
-                    secondary: "#32C5A3",
-                    accent: "#18232F",
-                    neutral: "#84cc16",
-                    "base-100": "#F5F9FF",
-                    info: "#0ea5e9",
-                    success: "#32C5A3",
-                    warning: "#facc15",
-                    error: "#e11d48",
-                },
-            },
-            "light",
-            "dark"
-        ],
-    },
-};
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: [
+//         // Or if using `src` directory:
+//         "./src/**/*.{js,ts,jsx,tsx,mdx}",
+//     ],
+//     theme: {
+//         fontFamily: {
+//             sans: ["Ubuntu", "sans-serif"],
+//         },
+//         extend: {
+//             colors: {
+//                 primary: "#003F52",
+//                 secondary: "#32C5A3",
+//                 white: "#FFFFFF",
+//                 "white-dark": "#F5F9FF",
+//                 black: "#18232F",
+//                 gray: {
+//                     50: "#EEF2F6",
+//                     100: "#DDE5EE",
+//                     200: "#BBCBDD",
+//                     300: "#9AB2CB",
+//                     400: "#7898BA",
+//                     500: "#456587",
+//                     600: "#344C65",
+//                     700: "#18232F",
+//                     800: "#111922",
+//                     900: "#090D11",
+//                 },
+//             },
+//             boxShadow: {
+//                 base: "0px 18px 50px -10px rgba(120, 152, 186, 0.3)",
+//             },
+//             borderRadius: {
+//                 "8xl": "3rem",
+//             },
+//             height: {
+//                 "90": "90%"
+//             }
+//         },
+//     },
+//     plugins: [require("daisyui")],
+//     daisyui: {
+//         themes: [
+//             {
+//                 vs: {
+//                     fontSize: "1rem",	
+//                     primary: "#003F52",
+//                     secondary: "#32C5A3",
+//                     accent: "#18232F",
+//                     neutral: "#84cc16",
+//                     "base-100": "#F5F9FF",
+//                     info: "#0ea5e9",
+//                     success: "#32C5A3",
+//                     warning: "#facc15",
+//                     error: "#e11d48",
+//                 },
+//             },
+//             "light",
+//             "dark"
+//         ],
+//     },
+// };
