327899f71d42eebe7991bdf2f54faa2209b06461
Carmelo Naim <carmelonaim30@gmail.com>
2024-04-07 13:47:46 -0400

    feat: post botQuestions services

--- src/actions/deleteActions.ts
+++ src/actions/deleteActions.ts
@@ -66,3 +66,34 @@ export const deleteAssingments = async (assingmentId: number) => {
       };
   }
 }
+
+export const deleteQuestions = async (questionId: string) => {
+  try {
+      const session = await getSession();
+      const response = await axios.delete(`${config.apiUrl}questions/${questionId}`, {
+          headers: {
+              Authorization: `Bearer ${session.token}`,
+          },
+      });
+
+      return {
+          status: response.status,
+          data: response.data,
+      };
+  } catch (error) {
+      if (axios.isAxiosError(error)) {
+          console.log("error message: ", error.response?.data.message);
+          return {
+              status: error.response?.status,
+              message: error.response?.data.message,
+          };
+      }
+
+      console.log("error: ", error);
+
+      return {
+          status: 500,
+          message: "Internal Server Error",
+      };
+  }
+}
