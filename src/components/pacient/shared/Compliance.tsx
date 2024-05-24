1db805606613a58513b0f88ef5b9d5edb5711976
Carmelo Naim <carmelonaim30@gmail.com>
2024-05-24 12:12:54 -0400

    compliance bug fixed

--- src/components/pacient/shared/Compliance.tsx
+++ src/components/pacient/shared/Compliance.tsx
@@ -101,7 +101,7 @@ const sendAlert = async(indication:Indication,type:string) => {
                       <TableRow key={indication.recordId}>
                         <TableCell>{indication.description}</TableCell>
                         <TableCell>{indication.completed ? indication.hour : '--'}</TableCell>
-                        <TableCell onClick={ () => sendAlert( indication, indication.completed ? 'warning': 'congrats' )} sx={{ textAlign: 'center' }}>
+                        <TableCell onClick={ () => sendAlert( indication, indication.completed ? 'congrats': 'warning' )} sx={{ textAlign: 'center' }}>
                         <Tooltip title={indication.completed ? 'Enviar Felicitaciones': 'Enviar Advertencia'} placement="right-start">
                           {indication.completed ? <CheckIcon style={{ color: 'green' }} /> : <DangerousIcon style={{ color: 'red' }} />}
                         </Tooltip>
